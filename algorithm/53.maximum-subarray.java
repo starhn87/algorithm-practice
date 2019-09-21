d0ad70c767e2deedcd943b8934b2c20c81d2993e
starhn87 <starhn87@gmail.com>
2019-09-21 09:36:08 +0900

    한글깨진 주석 수정

--- algorithm/53.maximum-subarray.java
+++ algorithm/53.maximum-subarray.java
@@ -13,7 +13,7 @@
  * Total Submissions: 1.4M
  * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
  *
- * Given an integer array nums, find the contiguous subarray (containing at
+ * Given an integer array nums, find the contiguous subarrayÂ (containing at
  * least one number) which has the largest sum and return its sum.
  * 
  * Example:
@@ -21,7 +21,7 @@
  * 
  * Input: [-2,1,-3,4,-1,2,1,-5,4],
  * Output: 6
- * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Explanation:Â [4,-1,2,1] has the largest sum = 6.
  * 
  * 
  * Follow up:
@@ -35,10 +35,10 @@ class Solution {
         int answer = nums[0], sum = nums[0];
         for (int i = 1; i < nums.length; i++) {
             if (0 < sum) {
-                // sum? 0?? ?? ?? ??? ??
+                // sum > 0이면 쓸모가 있음
                 sum += nums[i];
             } else {
-                // ???? ?? ??? ?
+                // sum <= 0이면 현재 값이 더 큼
                 sum = nums[i];
             }
             answer = Math.max(answer, sum);
