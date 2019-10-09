c2b4ebf9ffa14dc0382ba5aeda446e6d20b23089
starhn87 <starhn87@gmail.com>
2019-10-09 20:31:41 +0900

    leetcode database 197

---
+++ database/197.rising-temperature.sql
@@ -0,0 +1,21 @@
+-- Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
+-- +---------+------------------+------------------+
+-- | Id(INT) | RecordDate(DATE) | Temperature(INT) |
+-- +---------+------------------+------------------+
+-- |       1 |       2015-01-01 |               10 |
+-- |       2 |       2015-01-02 |               25 |
+-- |       3 |       2015-01-03 |               20 |
+-- |       4 |       2015-01-04 |               30 |
+-- +---------+------------------+------------------+
+-- For example, return the following Ids for the above Weather table:
+
+-- +----+
+-- | Id |
+-- +----+
+-- |  2 |
+-- |  4 |
+-- +----+
+
+-- # Write your MySQL query statement below
+SELECT W1.Id FROM Weather W1 JOIN Weather W2 ON DATEDIFF(W1.RecordDate, W2.RecordDate) = 1 AND W1.Temperature > W2.Temperature;
