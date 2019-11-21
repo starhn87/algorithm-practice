b3109f3e8664a6e6133c561d70c0004155e12d05
starhn87 <starhn87@gmail.com>
2019-11-21 12:12:38 +0900

    leetcode algorithm 20

---
+++ algorithm/20.valid-parentheses.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (37.15%)
+ * Likes:    3632
+ * Dislikes: 176
+ * Total Accepted:    759K
+ * Total Submissions: 2M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        while (s.contains("()") 
+                || s.contains("[]") 
+                || s.contains("{}")) {
+            s = s.replace("()", "")
+                 .replace("[]", "")
+                 .replace("{}", "");
+        }
+
+        return s.equals("");
+    }
+}
+// @lc code=end
+
